cbf3c865da9302a2dc94d7de36a9b1457424cecd
Andrew Burks <andrewtburks@gmail.com>
2019-04-09 15:52:44 -0500

    make some basic additions tothe webpage

--- docs/main.js
+++ docs/main.js
@@ -16,6 +16,8 @@ let sum1 = s.summarize(ex1);
 
 console.log(sum1);
 
+let data1elem = document.getElementById("data1");
 let sum1elem = document.getElementById("summary1");
 
+data1elem.innerHTML = JSON.stringify(ex1, null, ' ');
 sum1elem.innerHTML = s.printSummary(sum1);
