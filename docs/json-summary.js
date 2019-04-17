728181b7dd59b42241ffdd2ebcee98fe5c2c5a01
Andrew Burks <andrewtburks@gmail.com>
2019-04-17 12:15:16 -0500

    version up

--- docs/json-summary.js
+++ docs/json-summary.js
@@ -1,4 +1,4 @@
-// https://andrewtburks.dev/json-summary v0.2.3 Copyright 2019 Andrew Burks
+// https://andrewtburks.dev/json-summary v0.2.4 Copyright 2019 Andrew Burks
 (function (global, factory) {
 typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
 typeof define === 'function' && define.amd ? define(factory) :
